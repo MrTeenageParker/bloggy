94c80b699b9e4ceeaced29ad12067087f8273a52
Jacob Parker <Djmann1013@gmail.com>
2016-09-17 02:03:49 +0000

    Added index.html

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var fs=require('fs');
 
 //Lets define a port we want to listen to
 const PORT=80;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('<h1>Coming Soon!</h1>');
+   fs.readFile("index.html",function(error,data){
+       if(error){
+           response.writeHead(404,{"Content-type":"text/plain"});
+           response.end("Sorry the page was not found");
+       }else{
+           response.writeHead(202,{"Content-type":"text/html"});
+           response.end(data);
+       }
+   });
 }
 
 //Create a server
