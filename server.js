4bd6d481ff57e3994722baad12b7a4965e924d53
Jacob Parker <Djmann1013@gmail.com>
2016-09-17 05:21:03 +0000

    Revamped and added a more simpler and secure server.js!

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
-// Credit to user1937198 on StackOverflow for the response code
-var http = require('http');
-var fs = require('fs');
+// Stuff to require
+var express = require('express');
+var app = express();
+var path = require('path');
 
-// Port to listen to
-const PORT=80;
+// Configuration for the app
+app.use(express.static(path.join(__dirname, "public")));
+app.set("port", 80);
 
-function handleRequest(request, response){
-   fs.readFile("." + request.url,function(error,data){
-	if(request.url == "/"){
-	   fs.readFile("index.html",function(error,data){
-		response.writeHead(202,{"Content-type":"text/html"});
-           	response.end(data);
-	   });
-	} else {
-	   fs.readFile("."+request.url,function(error,file){
-		if(error){
-			response.writeHead(404,{"Content-type":"text/plain"});
-           		response.end("Sorry the page was not found");
-		} else {
-			if((request.url).match(/(gif|png|jpg|jpeg|ico)$/)){
-				response.writeHead(202,{"Content-type":"image"});
-                        	response.end(file,'binary');
-			} else {
-           			response.writeHead(202,{"Content-type":"text/html"});
-           			response.end(file);
-			}
-		}
-	   });
-	}
-   });
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function(){
-    // Add logging to a file for future reference?
-    console.log("Server listening on: http://localhost:%s", PORT);
+// Server stuff
+var server = app.listen(app.get("port"),function() {
+	var port = server.address().port;
+	console.log("Listening to port " + port + "!");
 });
